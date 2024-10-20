9f2e894542e7ad9994514a6b39d1f8bd4e941d03
huangjiaqi1997 <2931915202@qq.com>
2024-10-20 12:57:29 +0800

    Updates

--- assets/pay.1f22c872.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./index.bf3235d2.js";const p=()=>r({url:"/user/vip/pay/list"}),i=(s,a,t,e)=>r({url:"/user/alipay",params:{subject:s,totalAmount:a,body:t,isMobile:e}}),l=s=>r({url:"/sys/pay/result",params:{out_trade_no:s}});export{p as a,l as b,i as g};
